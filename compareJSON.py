cad7677c51d9cb5531ef93e548360d239330a6e0
Sebastian Lacki <sebastianlacki@Sebastians-MacBook-Pro-2.local>
2017-05-31 00:53:15 -0400

    Small update in recursive Comparitor

--- compareJSON.py
+++ compareJSON.py
@@ -1,21 +1,18 @@
 import json
 import sys
 
-def recursiveCheck(output1,output2):
-    for line in output1:
-
-        if line not in output1:
-            print("The attribute --"+line+"--doesn't exist in Output 1")
-        if line not in output2:
-            print("The attribute --"+line+"--doesn't exist in Output 2")
-
-        if output1[line] != output2[line]:
-            output1[line]
-            output2[line]
-            difference = 'Output mismatch @ --'+line+'--\nOutput 1: '+str(output1[line])+' Output 2: '+str(output2[line])+'\n'
-            print(difference)
-
 output1 = json.load(open(sys.argv[1]))
 output2 = json.load(open(sys.argv[2]))
+
+
+#Not recursiveCheck (yet) trying to figure out how to properly loop through...
+def recursiveCheck(output1,output2):
+    
+    for key in output1.keys():
+        if key not in output2:
+            print("The attribute --"+line+"--doesn't exist in Output 2")
+
+        if output1[key] != output2[key]:
+            print('\nOutput mismatch @ --'+key+'--\nOutput 1: '+str(output1[key])+' Output 2: '+str(output2[key])+'\n')
 
 recursiveCheck(output1,output2)
