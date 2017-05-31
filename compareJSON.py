1daecc401e306e2c84aea1ea72a733f592f27276
Sebastian Lacki <sebastianlacki@Sebastians-MacBook-Pro-2.local>
2017-05-31 01:49:56 -0400

    Working version of JSON file compare/diff. Prints a log of differences to the console and their positions in the JSON file as well as notifies the console of any missing key values if applicable...

--- compareJSON.py
+++ compareJSON.py
@@ -6,13 +6,18 @@ output2 = json.load(open(sys.argv[2]))
 
 
 #Not recursiveCheck (yet) trying to figure out how to properly loop through...
-def recursiveCheck(output1,output2):
-    
+def recursiveCheck(output1,output2,path):
     for key in output1.keys():
+        if type(output1[key]) == type(dict()):
+            if key not in output2:
+                print("\n--"+path+'[\''+key+"\']--doesn't exist in Output 2\n")
+            else:
+                recursiveCheck(output1[key],output2[key],path+'[\''+key+'\']')
+            continue
+
         if key not in output2:
-            print("The attribute --"+line+"--doesn't exist in Output 2")
+            print("\nKey path --"+path+'[\''+key+"\']-- doesn't exist in input file 2\n")
+        elif output1[key] != output2[key]:
+            print('\nOutput mismatch @ key path --'+path+'[\''+key+'\']--\nFile 1:\t'+str(output1[key])+'\tFile 2:\t'+str(output2[key])+'\n')
 
-        if output1[key] != output2[key]:
-            print('\nOutput mismatch @ --'+key+'--\nOutput 1: '+str(output1[key])+' Output 2: '+str(output2[key])+'\n')
-
-recursiveCheck(output1,output2)
+recursiveCheck(output1,output2,'')
